d25bfbb0b9bed6cee1cd6a6f7a1e130983de5787
Miriam Tocino <miriam.tocino@gmail.com>
2016-08-05 07:08:47 +0200

    Create a New Dinosaur Page

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -6,4 +6,8 @@ class DinosaursController < ApplicationController
   def show
     @dinosaur = Dinosaur.find(params[:id])
   end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "dinosaurs#index"
 
   get "dinosaurs" => "dinosaurs#index"
+  get 'dinosaurs/new' => "dinosaurs#new"
   get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
 end
