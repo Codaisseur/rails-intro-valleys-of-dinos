d31c0a54e451cb13355a57382e1dd454077a8213
Miriam Tocino <miriam.tocino@gmail.com>
2016-08-05 07:34:50 +0200

    Validations

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,4 +1,10 @@
 class Dinosaur < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 5 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
+
   def self.order_by_name
     order(:name)
   end
