6744c4f173e780a7eea763d2dbc07c21b2d5daa6
Miriam Tocino <miriam.tocino@gmail.com>
2016-08-05 07:12:29 +0200

    Create Dinosaur

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -10,4 +10,16 @@ class DinosaursController < ApplicationController
   def new
     @dinosaur = Dinosaur.new
   end
+
+  def create
+    dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
+
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+    if @dinosaur.save
+       redirect_to @dinosaur
+    else
+       render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "dinosaurs#index"
 
   get "dinosaurs" => "dinosaurs#index"
-  get 'dinosaurs/new' => "dinosaurs#new"
+  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
   get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+  post "dinosaurs" => "dinosaurs#create"
 end
