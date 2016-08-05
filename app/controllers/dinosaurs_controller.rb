522877769675a1f78c68e71f81bbe25fcc697c39
Miriam Tocino <miriam.tocino@gmail.com>
2016-08-05 07:02:58 +0200

    Create a Show Dinosaur Page

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -2,4 +2,8 @@ class DinosaursController < ApplicationController
   def index
     @dinosaurs = Dinosaur.all
   end
+
+  def show
+    @dinosaur = Dinosaur.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "dinosaurs#index"
 
   get "dinosaurs" => "dinosaurs#index"
+  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
 end
